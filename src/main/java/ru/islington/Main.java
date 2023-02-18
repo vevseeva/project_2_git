1df72f051c37adc8679bca85e5b1a706edc94e35
vevseeva <125492378+vevseeva@users.noreply.github.com>
2023-02-18 21:53:05 +0300

    Update Main.java

--- src/main/java/ru/islington/Main.java
+++ src/main/java/ru/islington/Main.java
@@ -2,7 +2,7 @@ package ru.islington;
 
 public class Main {
     
-    private static String msg = ""Goodbye, qa.guru!"";
+    private static String msg = "Goodbye, qa.guru!";
     
     public static void main(String[] args) {
         System.out.println(msg);
