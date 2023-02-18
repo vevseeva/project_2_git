cafcd803911818ce4277ca688da6d285001850f2
vevseeva <125492378+vevseeva@users.noreply.github.com>
2023-02-18 21:46:06 +0300

    Update Main.java

--- src/main/java/ru/islington/Main.java
+++ src/main/java/ru/islington/Main.java
@@ -2,6 +2,6 @@ package ru.islington;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello qa.guru");
+        System.out.println("Goodbye qa.guru");
     }
 }
